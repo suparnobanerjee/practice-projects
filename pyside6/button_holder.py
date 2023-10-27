25f1a7c193c31c9840395cb5ec34fa9759593e23
Suparno Banerjee <suparnobanerjee011@gmail.com>
2023-10-27 08:57:21 +0530

    added code

---
+++ pyside6/button_holder.py
@@ -0,0 +1,8 @@
+from PySide6.QtWidgets import QMainWindow, QPushButton
+
+class ButtonHolder(QMainWindow):
+  def __init__(self):
+    super().__init__()
+    self.setWindowTitle("Button Holder App")
+    button = QPushButton("Press me!")
+    self.setCentralWidget(button)
--- pyside6/main.py
+++ pyside6/main.py
@@ -1,24 +1,6 @@
-from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton
+from PySide6.QtWidgets import QApplication
 import sys
-
-# # Without OOP
-# app = QApplication(sys.argv)
-
-# window = QMainWindow()
-# window.setWindowTitle("Main Window App :)")
-# button = QPushButton()
-# button.setText("Press this button!")
-# window.setCentralWidget(button)
-# window.show()
-# app.exec()
-
-# With OOP
-class ButtonHolder(QMainWindow):
-  def __init__(self):
-    super().__init__()
-    self.setWindowTitle("Main Window App :)")
-    button = QPushButton("Press Me!")
-    self.setCentralWidget(button)
+from button_holder import ButtonHolder
 
 if __name__=="__main__":
   app = QApplication(sys.argv)
