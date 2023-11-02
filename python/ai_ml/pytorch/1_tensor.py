9064dc94a68e90d81ee85d3c32a1e98f7ac8d4f9
Suparno Banerjee <suparnobanerjee011@gmail.com>
2023-11-02 21:29:35 +0530

    added code

--- python/ai_ml/pytorch/1_tensor.py
+++ python/ai_ml/pytorch/1_tensor.py
@@ -17,10 +17,23 @@ import numpy as np
 
 # MATRIX = torch.tensor([[1,2,3],[4,5,6]]) # 2-D Tensor
 
-TENSOR = torch.tensor([[[1,2,4],[3,4,6],[0,0,0]],[[10,5,6],[0,7,8],[9,9,9]],[[9,10,11],[100,11,12],[10,10,10]],[[0,0,1],[1,2,3],[5,6,10]]])
-print(TENSOR)
-print(TENSOR.ndim)
-print(TENSOR.shape)
+# TENSOR = torch.tensor([[[1,2,4],[3,4,6]],[[0,7,8],[9,9,9]],[[9,10,11],[100,11,12]],[[0,0,1],[1,2,3]]])
+# print(TENSOR)
+# print(TENSOR.ndim)
+# print(TENSOR.shape)
 
+# random = torch.rand(4,3) # Random tensors are important in the sense that neural nets tends to learn from tensors full of random numbers and adjust those random numbers to better represent the data.
+# print(random)
+# print(random.ndim)
 
+# zeros = torch.zeros(2,3) # same for ones
+# print(zeros)
+# print(zeros.dtype)
 
+# zero_to_ten = torch.arange(1,10)
+# zero_to_ten = torch.arange(start=1,end=10,step=2)
+# print(zero_to_ten)
+# to_zeros = torch.zeros_like(input=zero_to_ten)
+# print(to_zeros)
+
+ 
