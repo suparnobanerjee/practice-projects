def9e4b1d80b86b20bd6eb5d457337e9c324c25c
Suparno Banerjee <suparnobanerjee011@gmail.com>
2024-06-08 13:06:49 +0530

    small update

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 - C++, Python, JavaScript, React, NextJS
 - Data Structures and Algorithms
 - AI/ML with primary focus on UL and RL
-- GUI modules - PySide6 and QT
+
---
+++ c++/binary search/basic.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int BinarySearch(int arr, int size, int key){
+  int start=0;
+  int end=size-1;
+  int mid=(start+end)/2;
+  while(end>=start){
+    if(key==mid){
+      
+    }
