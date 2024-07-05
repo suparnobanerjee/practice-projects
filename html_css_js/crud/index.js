80c6498b5790c93b85111840bbb541ac0fa67d3a
Suparno Banerjee <suparnobanerjee011@gmail.com>
2024-07-05 20:45:52 +0530

    first commit

--- html_css_js/crud/index.js
+++ html_css_js/crud/index.js
@@ -1,10 +1,28 @@
 const express=require('express');
 const app=express();
+const mongoose=require('mongoose');
+const Product=require('./models/product.model.js');
+const { configDotenv } = require('dotenv');
+configDotenv();
+const productRoute=require('./routes/product.route.js');
 
-app.listen(3000,()=>{
-  console.log("Server is running!");
+//middleware
+app.use(express.json());
+
+//routes
+app.use('/api/products',productRoute);
+
+app.get("/", (req, res) => {
+  res.send("Hello from Node API Server Updated!");
 });
 
-app.get('/',(req,res)=>{
-  res.send("Hello from Node API");
+mongoose.connect(process.env.MONGODB_URL).then(()=>{
+  console.log("Connected to database!");
+  app.listen(3000,()=>{
+    console.log("Server is running on port 3000");
+  });
+})
+.catch(()=>{
+  console.log("Connection to DB failed!");
 });
+
---
+++ html_css_js/crud/models/product.model.js
@@ -0,0 +1,28 @@
+const mongoose=require('mongoose');
+
+const productSchema=new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Enter product name"]
+  },
+  quantity: {
+    type: String,
+    required: true,
+    default: 0
+  },
+  price: {
+    type: Number,
+    required: true,
+    default: 0
+  },
+  image: {
+    type: String,
+    required: false
+  }
+},{
+  timestamps: true
+});
+
+const Product=mongoose.model('Product', productSchema);
+
+module.exports=Product;
